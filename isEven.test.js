ee39856991a3dfea8bbb963b4608d3dda6f9688f
Leonardo Courbassier Martins <LeoCourbassier@users.noreply.github.com>
2020-03-06 03:50:01 -0300

    Updated test for the positive integers

--- isEven.test.js
+++ isEven.test.js
@@ -4,9 +4,15 @@ const notFunIsEven = n => n % 2 == 0 && n >= 0;
 
 describe('isEven function', () => {
   describe('Tests for the positive integers', () => {
+    for (let i = 0; i < 100; i++) {
+      let random = Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+      let ans = notFunIsEven(random);
       it('isEven returns ' + ans + ' for ' + random, () => {
+        expect(isEven(random)).toBe(ans);
+      });
+    }
   });
-}
+
   describe('Tests for the negative integers', () => {
   describe('Tests for the fractionals numbers', () => {
     }
