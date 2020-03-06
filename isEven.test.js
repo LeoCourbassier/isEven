fe5e346073fd454264cb19bdabe89d1b70bfb698
Leonardo Courbassier Martins <LeoCourbassier@users.noreply.github.com>
2020-03-06 03:56:23 -0300

    Now using a envvar to know how many repetitions we want

--- isEven.test.js
+++ isEven.test.js
@@ -2,9 +2,11 @@ const isEven = require('./isEven');
 
 const notFunIsEven = n => n % 2 == 0 && n >= 0;
 
+const numberOfTimes = process.env['isEven_repetition_times'] || 100;
+
 describe('isEven function', () => {
   describe('Tests for the positive integers', () => {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < numberOfTimes; i++) {
       let random = Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
       let ans = notFunIsEven(random);
       it('isEven returns ' + ans + ' for ' + random, () => {
@@ -14,7 +16,7 @@ describe('isEven function', () => {
   });
 
   describe('Tests for the negative integers', () => {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < numberOfTimes; i++) {
       let random = -Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
       let ans = notFunIsEven(random);
       it('isEven returns ' + ans + ' for ' + random, () => {
@@ -24,7 +26,7 @@ describe('isEven function', () => {
   });
 
   describe('Tests for the fractionals numbers', () => {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < numberOfTimes; i++) {
       let random = Math.random();
       let ans = notFunIsEven(random);
       it('isEven returns ' + ans + ' for ' + random, () => {
